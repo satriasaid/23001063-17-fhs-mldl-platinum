5919134b5ef443396549f0691b305c7c01b7b02b
satriasaid <satriasaid@gmail.com>
2024-03-24 22:58:34 +0700

    add related files and updated the code

--- platinumchcode.py
+++ platinumchcode.py
@@ -42,6 +42,73 @@ def hello_world():
     response_data = jsonify(json_response)
     return response_data
 
+# proses Neural Network untuk data mula-mula 
+
+# 1. loading initial data
+df = pd.read_csv("train_preprocess.tsv", sep='\t', names=["tweet", "sentiment"])
+df_proc = df.copy()
+
+# 2. melakukan cleaning pada initial data
+def cleaning(data):
+    data = data.replace(r'(\\x\S+)','',regex=True).astype('str')
+    data = data.replace(r'((\s{2}))',' ',regex=True).astype('str')
+    data = data.replace(r'(\\n)',' ',regex=True).astype('str')
+    data = data.replace(r'(?:RT\W+)',' ',regex=True).astype('str')
+    data = data.replace(r'(?:USER\W+)',' ',regex=True).astype('str')
+    data = data.replace(r'(?:USER)',' ',regex=True).astype('str')
+    data = data.replace(r'(?:https:)\S+',' ',regex=True).astype('str')
+    data = data.replace(r'(?:http)\S+',' ',regex=True).astype('str')
+    data = data.replace(r'(?:https)\S+',' ',regex=True).astype('str')
+    data = data.replace(r'(?:URL)',' ',regex=True).astype('str')
+    data = data.replace(r'[^0-9A-Za-z]',' ',regex=True).astype('str')
+    cleanTwList = data.to_list()
+    cleanTwList = [x.strip(' ') for x in cleanTwList] #menghilangkan spasi yang tidak penting
+    cleanTwList = [re.sub(r'\s+',' ',x) for x in cleanTwList] #menghilangkan spasi yang berlebihan
+    data = cleanTwList
+    return data
+
+temp_col = cleaning(df_proc['tweet'])
+df_proc['tweet'] = temp_col
+
+df_alay = pd.read_csv("/external_source/kbba.txt", sep='\t', names=['alay','normal'])
+df_sw = pd.read_csv("/external_source/stopword.txt", sep='\t', names=['stopword'])
+df_neg = pd.read_csv("/external_resource/negation_combi.txt", sep='\t', names=['negate'])
+
+alay_list = df_alay['alay'].to_list()
+normal_list = df_alay['normal'].to_list()
+sw_list = df_sw['stopword'].to_list()
+neg_list = df_neg['negate'].to_list()
+
+for swli in sw_list:
+    if swli in neg_list:
+        sw_list.remove(swli)
+        
+sw_list.append('nya')
+
+def txt_dealay(datafr):
+    data = datafr.str.lower()
+    for i in range(len(alay_list)):
+        normal = data.replace(r'\b'+ alay_list[i] + r'\b',normal_list[i],regex=True).astype('str')
+        i += 1
+        data = normal
+    return data
+
+def txt_sw(datafr):
+    data = datafr.str.lower()
+    for i in range(len(sw_list)):
+        normal = data.replace(r'\b'+ sw_list[i] + r'\b','',regex=True).astype('str')
+        i += 1
+        data = normal
+    return data
+
+df_proc['norm'] = txt_dealay(df_proc['tweet'])
+df_proc['norm'] = txt_sw(df_proc['norm'])
+df_proc['norm'] = cleaning(df_proc['norm'])
+df_proc[['tweet','norm','sentiment']]
+    
+# 3. Melakukan Feature Extraction untuk Neural Network
+
+
 # Endpoint 1 untuk Neural Network menerima teks
 
 # Endpoint 2 untuk Neural Network menerima file
